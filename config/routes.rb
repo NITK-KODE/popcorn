e7dd7c014c8ebc11b6184b1d8e62e7a5fa161dea
cjphonehomec@gmail.com <cjphonehomec@gmail.com>
2021-03-28 15:19:22 +0530

    fixed basic things

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/feed' => 'users#feed'
   put '/posts/:id/like' => 'posts#like' , as: 'like'
   post '/posts/:id/comment' => 'posts#comment' , as:'comment'
+  get '/me' => 'users#index' , as: "profile"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#feed'
   resources :posts , only:[:show]
