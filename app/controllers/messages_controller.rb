5f1e54813d21b8f2dd481a4dd89c141887b4ab25
cjphonehomec@gmail.com <cjphonehomec@gmail.com>
2021-04-01 21:51:19 +0530

    working

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,14 @@
+class MessagesController < ApplicationController
+    def new 
+        @message = Message.new
+    end
+
+    def create
+      
+            @message = Message.create(user_id:current_user.id , message:params[:content])
+            if @message.save 
+                ActionCable.server.broadcast 'room_channel' , {content:"test content" , user_id: 54}
+          
+        end
+    end
+end
