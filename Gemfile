d25b4ddcc612e20d06c101d1adb334d952b37fc5
cjphonehomec@gmail.com <cjphonehomec@gmail.com>
2021-04-13 01:53:28 +0530

    working....

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'sass-rails', '>= 6'
 gem 'devise'
 gem 'jquery-rails'
 gem 'zodiac'
-
+gem "will_paginate-bootstrap"
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', '~> 5.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_173657) do
+ActiveRecord::Schema.define(version: 2021_04_12_190355) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2021_04_12_173657) do
     t.string "twitter"
     t.string "facebook"
     t.string "description"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
