27aec9e35b2967e7ec88e16234c743738664f886
cjphonehomec@gmail.com <cjphonehomec@gmail.com>
2021-04-15 15:29:33 +0530

    testing mail...1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,7 @@ Rails.application.configure do
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
+  
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -61,6 +62,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "popcorn_production"
+  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'} 
 
   config.action_mailer.perform_caching = false
 
