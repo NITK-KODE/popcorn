682bcf49423b5f6ba9add9519ea29acb65815416
cjphonehomec@gmail.com <cjphonehomec@gmail.com>
2021-04-20 16:27:12 +0530

    mail support1

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'limitless-ocean-51623.heroku.com',
+  :domain         => ENV['MAILGUN_DOMAIN'],
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
