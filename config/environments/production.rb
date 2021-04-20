65f56b82a33ec2ef146aaecb9ef760acc8abf06a
cjphonehomec@gmail.com <cjphonehomec@gmail.com>
2021-04-20 18:49:27 +0530

    stage  completed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "popcorn_production"
-  config.action_mailer.default_url_options = { :host => 'limitless-ocean-51623.herokuapp.com'} 
+  config.action_mailer.default_url_options = { :host => 'https://limitless-ocean-51623.herokuapp.com'} 
   
   config.action_mailer.perform_caching = false
 
