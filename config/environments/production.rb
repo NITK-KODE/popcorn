10ded678ea78071c9ffd29c97e14a6481c138433
cjphonehomec@gmail.com <cjphonehomec@gmail.com>
2021-04-20 18:42:57 +0530

    stage  completed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "popcorn_production"
-  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'} 
-
+  config.action_mailer.default_url_options = { :host => 'limitless-ocean-51623.herokuapp.com'} 
+  
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
